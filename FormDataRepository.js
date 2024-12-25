65c62cdbc82a6a652706ba747057e09f89041086
violentidk <161117561+violentidk@users.noreply.github.com>
2024-12-25 18:55:43 +0200

    Create FormDataRepository.js

---
+++ FormDataRepository.js
@@ -0,0 +1,7 @@
+// src/main/java/com/example/webapp/FormDataRepository.java
+package com.example.webapp;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FormDataRepository extends JpaRepository<FormData, Long> {
+}
